1b342da366ee4f490b840e3f33a193ea6cd45dbe
thewoz <leonardo.parisi@gmail.com>
2023-12-01 12:08:29 +0100

    Update test_cameracalibration.cpp

--- modules/calib3d/test/test_cameracalibration.cpp
+++ modules/calib3d/test/test_cameracalibration.cpp
@@ -1400,6 +1400,10 @@ void CV_StereoCalibrationTest::run( int )
             imgsize = left.size();
             bool found1 = findChessboardCorners(left, patternSize, imgpt1[i]);
             bool found2 = findChessboardCorners(right, patternSize, imgpt2[i]);
+          
+            cv::cornerSubPix(left,  imgpt1[i], cv::Size(6,6), cv::Size(-1,-1), cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 15, 0.1));
+            cv::cornerSubPix(right, imgpt2[i], cv::Size(6,6), cv::Size(-1,-1), cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 15, 0.1));
+
             if(!found1 || !found2)
             {
                 ts->printf( cvtest::TS::LOG, "The function could not detect boards (%d x %d) on the images %s and %s, testcase %d\n",
